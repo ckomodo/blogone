ea2dae962d420d3269832e171f62b9b4c9d293ef
ckomodo <chrissakwa@gmail.com>
2021-02-28 21:38:32 -0800

    added a booklist function and rendered two different books

--- src/index.js
+++ src/index.js
@@ -1,42 +1,48 @@
 import React from "react";
 import ReactDom from "react-dom";
- import './index.css';
+import "./index.css";
 
+// variables
 
+const firstBook = {
+  img:
+    "https://m.media-amazon.com/images/I/81fO6by7-nL._AC_UL640_FMwebp_QL65_.jpg",
+  title: "The Dancing Girls",
+  author: "M. M. Chouinard",
+};
 
-
+const secondBook = {
+  img:
+    "https://m.media-amazon.com/images/I/61EYPTVI2JL.jpg",
+  title: "The Return",
+  author: "Nicholas Sparks, Kyf Brewer, et al.",
+};
 
 function BookList() {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-    
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book 
+      img={secondBook.img}
+      title={secondBook.title}
+      author={secondBook.author}/>
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
-    <article>
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
-     
+    <article className="bookname">
+      <img src={props.img} alt=" " />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/81fO6by7-nL._AC_UL640_FMwebp_QL65_.jpg"
-    alt=" "
-  />
-);
-
-const Title = () => <h1>The Dancing Girls</h1>
-const Author = () => <h1>M. M. Chouinard</h1>
-
 
 ReactDom.render(<BookList />, document.getElementById("root"));
